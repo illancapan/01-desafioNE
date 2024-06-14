1c288617dda199998dd4949726d921aa24c2b744
illancapan <igorllancapan@gmail.com>
2024-06-14 09:25:09 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const { registrar, leer } = require('./operaciones.js');
+
+const [operacion, nombre, edad, animal, color, enfermedad] = process.argv.slice(2);
+
+if (operacion === 'registrar') {
+    registrar(nombre, edad, animal, color, enfermedad);
+    console.log("Cita registrada exitosamente.");
+} else if (operacion === 'leer') {
+    leer();
+} else {
+    console.log('Operación no reconocida. Usa "registrar" para añadir una nueva cita o "leer" para leer las citas.');
+}
---
+++ operaciones.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+const registrar = (nombre, edad, animal, color, enfermedad) => {
+  const citas = JSON.parse(fs.readFileSync("citas.json", "utf8"));
+  const id = Math.floor(Math.random() * 100).toString();
+
+  if (!nombre || !edad || !animal || !color || !enfermedad) {
+    console.log("Por favor ingresa todos los datos");
+    return;
+  }
+  citas.push({ id, nombre, edad, animal, color, enfermedad });
+  fs.writeFileSync("citas.json", JSON.stringify(citas));
+};
+
+const leer = () => {
+  const citas = JSON.parse(fs.readFileSync("citas.json", "utf8"));
+  console.log(citas);
+};
+
+module.exports = { registrar, leer };
